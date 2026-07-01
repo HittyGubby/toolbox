f0b5c0a55b1be81aeedc69c959b1d0960c9b364f
HittyGubby <HitGub@outlook.com>
2026-07-01 11:40:43 +0800

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: "",
@@ -10,6 +11,40 @@ export default defineConfig({
       compilerOptions: {
         runes: ({ filename }) =>
           filename.includes("node_modules") ? undefined : true,
+      },
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "Toolbox",
+        short_name: "Toolbox",
+        description:
+          "Static Instant SPA AIO toolbox that doesnt fuck your privacy",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*"],
       },
     }),
   ],
